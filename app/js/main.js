c6ef5569e85fe3526858b6cb50b0e6ca5ace715a
Kelley Rose <krose422@gmail.com>
2015-06-12 15:33:40 -0400

    Added feature - click on left list item to populate selected contact

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,8 @@ var email = $('#emailAdd');
 var phone = $('#phoneNum');
 var website = $('#website');
 var notes = $('#notes');
+var addressOne = $('#addOne');
+var addressTwo = $('#addTwo');
 
 // Add to collection
 var allContacts = new ContactCollection();
@@ -17,6 +19,7 @@ var allContacts = new ContactCollection();
 allContacts.fetch().done( function () {
   allContacts.each( function (model) {
     addContactToList(model.attributes);
+    addContactToSC(model.attributes);
   });
 });
 
@@ -31,24 +34,63 @@ var addContact = function (e) {
       email: email.val(),
       phone: phone.val(),
       website: website.val(),
-      notes: notes.val()
+      notes: notes.val(),
+      addressOne: addressOne.val(),
+      addressTwo: addressTwo.val()
   });
 
   allContacts.add(contact).save().success( function (data) {
     addContactToList(data);
+    addContactToSC(data);
   });
 
   this.reset();
 };
 
+// Add contact function
 var addContactToList = function (d) {
   var nameListTemplate = template.nameList(d);
-
   $('#leftList').prepend(nameListTemplate);
+};
+
+var addContactToSC = function (d) {
+  var selectedContactTemplate = template.contactInfo(d);
+  $('#selectedContact').html(selectedContactTemplate);
+};
+
+// Delete contact function
+var deleteContact = function () {
+  var contactToDelete = $(this).parent(),
+      idToDelete = contactToDelete.attr('id'),
+      contactToDeleteList = $('.leftList').find('[data-id=' + idToDelete + ']');
+
+  allContacts.get(idToDelete).destroy().success( function (data) {
+    contactToDelete.remove();
+    contactToDeleteList.remove();
+    $('.selectedContact').html('<div class="deletedResponse"><p>Got rid of that one!</p><i class="fa fa-magic"></i></div>');
+  });
+};
+
+// Populate selected contact function
+var populateSC = function () {
+  var idToPopulate = $(this).data('id');
+
+  allContacts.each( function (contact) {
+    if (idToPopulate === contact.id) {
+      var scTemplate = template.contactInfo(contact);
+      $('.selectedContact').empty();
+      $('.selectedContact').html(template.contactInfo(contact.toJSON()));
+    }
+  });
 };
 
 // Form submit listener
 $('#contactInput').on('submit', addContact);
 
+// Delete event listener
+$('.main').on('click','#delete', deleteContact);
+
+// List item event listener
+$('ul').on('click', 'li', populateSC);
 
 // }());
--- app/js/templates.js
+++ app/js/templates.js
@@ -1,20 +1,32 @@
 this["template"] = this["template"] || {};
 this["template"]["contactInfo"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
-},"useData":true});
-this["template"] = this["template"] || {};
-this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<h1>\n  "
-    + this.escapeExpression(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
-    + "\n</h1>\n";
+  return "        <div class=\"sc\" id=\""
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
+    + "\">\n          <div class=\"sc-fullname\" id=\"sc-fullname\">\n            <p>"
+    + alias3(((helper = (helper = helpers.firstName || (depth0 != null ? depth0.firstName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"firstName","hash":{},"data":data}) : helper)))
+    + " "
+    + alias3(((helper = (helper = helpers.lastName || (depth0 != null ? depth0.lastName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"lastName","hash":{},"data":data}) : helper)))
+    + "</p>\n          </div>\n\n          <div class=\"sc-top\">\n\n            <div class=\"sc-avatar\" id=\"sc-avatar\">\n              <img src=\"images/User.png\">\n            </div>\n\n            <div class=\"sc-topright\">\n              <div class=\"phone\" id=\"sc-phone\">\n                <p><strong>Phone Number: </strong>"
+    + alias3(((helper = (helper = helpers.phone || (depth0 != null ? depth0.phone : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"phone","hash":{},"data":data}) : helper)))
+    + "</p>\n              </div>\n\n              <div class=\"email\" id=\"sc-email\">\n                <p><strong>Email address: </strong><a href=\"#\">"
+    + alias3(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"email","hash":{},"data":data}) : helper)))
+    + "</a></p>\n              </div>\n\n              <div class=\"sc-website\">\n                <p><strong>Website: </strong><a href=\"#\">"
+    + alias3(((helper = (helper = helpers.website || (depth0 != null ? depth0.website : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"website","hash":{},"data":data}) : helper)))
+    + "</a></p>\n              </div>\n            </div> <!-- end div topright -->\n\n          </div> <!-- end div sc-top -->\n\n          <div class=\"sc-bottom\">\n\n            <div class=\"sc-bottomleft\">\n              <div class=\"sc-address\" id=\"sc-address\">\n                <p>"
+    + alias3(((helper = (helper = helpers.addressOne || (depth0 != null ? depth0.addressOne : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"addressOne","hash":{},"data":data}) : helper)))
+    + "</p>\n                <p>"
+    + alias3(((helper = (helper = helpers.addressTwo || (depth0 != null ? depth0.addressTwo : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"addressTwo","hash":{},"data":data}) : helper)))
+    + "</p>\n              </div>\n\n\n            </div> <!-- end sc-bottomleft div -->\n\n            <div class=\"googlemap\">\n\n            </div>\n\n          </div> <!-- end sc-bottom div -->\n\n          <div id=\"delete\" class=\"delete\">\n            <span>Delete Contact</span>\n          </div>\n        </div>\n";
 },"useData":true});
 this["template"] = this["template"] || {};
 this["template"]["nameList"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<li class=\"indName\" id=\"indName\">\n  "
+  return "<li class=\"indName\" data-id=\""
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
+    + "\">\n  "
     + alias3(((helper = (helper = helpers.firstName || (depth0 != null ? depth0.firstName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"firstName","hash":{},"data":data}) : helper)))
     + " "
     + alias3(((helper = (helper = helpers.lastName || (depth0 != null ? depth0.lastName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"lastName","hash":{},"data":data}) : helper)))
