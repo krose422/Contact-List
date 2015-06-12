0b1b02a410de109469e6e2ffebac00fc3cd520fc
Kelley Rose <krose422@gmail.com>
2015-06-11 21:44:10 -0400

    Styled left sidebar

--- app/js/templates.js
+++ app/js/templates.js
@@ -1,8 +1,16 @@
 this["template"] = this["template"] || {};
+this["template"]["contactInfo"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+this["template"] = this["template"] || {};
 this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
   return "<h1>\n  "
     + this.escapeExpression(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
     + "\n</h1>\n";
+},"useData":true});
+this["template"] = this["template"] || {};
+this["template"]["nameList"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<li class=\"indName\" id=\"indName\">\n  Testing\n</li>\n";
 },"useData":true});
